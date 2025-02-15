8a0bc615c1b671bae341d584c6689426435d321c
Barys <brskmrvdcz@gmail.com>
2025-02-15 11:50:17 +0100

    normalized movement input

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,70 @@
 use raylib::prelude::*;
-use raylib::ffi::{RenderTexture, LoadRenderTexture};
+
+const SCREEN_WIDTH: f32 = 640f32;
+const SCREEN_HEIGHT: f32 = 480f32;
+
+struct Ball {
+    direction: Vector2,
+    position: Vector2,
+    speed: f32,
+    radius: f32,
+    color: Color,
+}
 
 fn main() {
-    
     let (mut rl, thread) = raylib::init()
-        .size(240,320)
-        //.resizable()
-        .title("SSFv3s_2R")
+        .size(SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32)
+        .title("Keyboard input")
+        .vsync()
         .build();
-    
-    rl.set_target_fps(60u32);
-    rl.set_window_min_size(240i32, 320i32);
- 
-    let game_screen_height= 240i32;
-    let game_screen_width= 320i32;
-    
-    let game_screen: RenderTexture = unsafe {
-        LoadRenderTexture(game_screen_width, game_screen_height)
+
+    let mut ball = Ball {
+        direction: Vector2::new(0f32, 0f32),
+        position: Vector2::new(SCREEN_WIDTH / 2f32, SCREEN_HEIGHT / 2f32),
+        speed: 120f32,
+        radius: 5f32,
+        color: Color::RED,
     };
-    
-    let mut position: Vector2 = Vector2::new(rl.get_screen_height() as f32 / 2f32, rl.get_screen_width() as f32 / 2f32);
 
     while !rl.window_should_close() {
         let delta_time = rl.get_frame_time();
 
-        if rl.is_key_pressed(KeyboardKey::KEY_F11){
-            rl.toggle_fullscreen();
+        /* --- UPDATE --- */
+
+        // Handle ball movement
+        {
+            let mut direction: Vector2 = Vector2::new(0f32, 0f32);
+            if rl.is_key_down(KeyboardKey::KEY_UP) {
+                direction.y -= 1f32;
+            }
+            if rl.is_key_down(KeyboardKey::KEY_DOWN) {
+                direction.y += 1f32;
+            }
+            if rl.is_key_down(KeyboardKey::KEY_LEFT) {
+                direction.x -= 1f32;
+            }
+            if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
+                direction.x += 1f32;
+            }
+            direction.normalize();
+            
+            // saving direction into struct to use when drawing sprite
+            // struct may not have own direction, if will not be used further
+            ball.direction = direction; 
+
+            ball.position += ball.direction * ball.speed * delta_time;
         }
 
-        // UPDATE
-        {
-            // Updating code here
-            if rl.is_key_down(KeyboardKey::KEY_UP){
-                position.y -= 100f32 * delta_time;
-            }
-            if rl.is_key_down(KeyboardKey::KEY_DOWN){
-                position.y += 100f32 * delta_time;
-            }
-            if rl.is_key_down(KeyboardKey::KEY_RIGHT){
-                position.x += 100f32 * delta_time;
-            }
-            if rl.is_key_down(KeyboardKey::KEY_LEFT){
-                position.x -= 100f32 * delta_time;
-            }
-        }
-        // DRAW
+        /* --- DRAW --- */
         let mut d = rl.begin_drawing(&thread);
+
+        // Handle ball drawing
         {
-            d.clear_background(Color::BLACK);
-            d.draw_circle(position.x as i32, position.y as i32, 3f32, Color::RED);
-            d.draw_text("Hello SSFv3s", 12i32, 12i32, 32i32, Color::RAYWHITE);
+            d.draw_circle_v(ball.position, ball.radius + 2f32, ball.color);
+            d.draw_circle_v(ball.position, ball.radius, Color::WHITE);
         }
+
+        d.clear_background(Color::BLACK);
+        d.draw_text("Hello world!", 12i32, 12i32, 24i32, Color::RAYWHITE);
     }
 }
