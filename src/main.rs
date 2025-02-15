845680568d28ca7354b37cb2c771742f2fb279e9
Barys <brskmrvdcz@gmail.com>
2025-02-15 14:09:44 +0100

    Texture handling + struct implementation

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,32 @@ struct Ball {
     speed: f32,
     radius: f32,
     color: Color,
+    sprite: Texture2D,
+}
+
+impl Ball {
+    pub fn new(rl: &mut RaylibHandle, thread: &RaylibThread, filepath: &str) -> Self {
+        Self {
+            direction: Vector2::new(0f32, 0f32),
+            position: Vector2::new(SCREEN_WIDTH / 2f32, SCREEN_HEIGHT / 2f32),
+            speed: 120f32,
+            radius: 5f32,
+            color: Color::RED,
+            sprite: rl.load_texture(thread, filepath).unwrap(),
+        }
+    }
+
+    pub fn draw(&self, d: &mut RaylibDrawHandle) {
+        d.draw_texture_v(
+            &self.sprite,
+            self.position
+                - Vector2::new(
+                    self.sprite.width() as f32 / 2f32,
+                    self.sprite.height() as f32 / 2f32,
+                ),
+            self.color,
+        );
+    }
 }
 
 struct BouncingBall {
@@ -30,21 +56,19 @@ fn main() {
 
     let mut value: i32 = rl.get_random_value(-100i32..100i32); // not right documentation
     let mut frame_count = 0;
+    let mut is_colliding: bool = false;
 
-    let mut ball = Ball {
-        direction: Vector2::new(0f32, 0f32),
-        position: Vector2::new(SCREEN_WIDTH / 2f32, SCREEN_HEIGHT / 2f32),
-        speed: 120f32,
-        radius: 5f32,
-        color: Color::RED,
-    };
-    
+    let mut ball = Ball::new(&mut rl, &thread, "assets/tree_left.png");
+
     let mut bouncing_ball = BouncingBall {
         position: Vector2::new(SCREEN_WIDTH / 2f32, SCREEN_HEIGHT / 2f32),
         velocity: Vector2::new(200f32, 200f32),
         radius: 5f32,
-        color: Color::BLUE
+        color: Color::BLUE,
     };
+
+    let texture_ground = rl.load_texture(&thread, "assets/ground.png").unwrap();
+    let texture_tree = rl.load_texture(&thread, "assets/tree_right.png").unwrap();
 
     while !rl.window_should_close() {
         let delta_time = rl.get_frame_time();
@@ -53,21 +77,31 @@ fn main() {
 
         // Checking collisios
         {
-            if check_collision_circles(ball.position, ball.radius, bouncing_ball.position, bouncing_ball.radius){
+            if check_collision_circles(
+                ball.position,
+                ball.radius,
+                bouncing_ball.position,
+                bouncing_ball.radius,
+            ) {
                 bouncing_ball.velocity *= -1f32;
+                is_colliding = true;
+            } else {
+                is_colliding = false;
             }
         }
 
         // Bouncing Ball
         {
             bouncing_ball.position += bouncing_ball.velocity * delta_time;
-            
+
             if bouncing_ball.position.x >= SCREEN_WIDTH - bouncing_ball.radius
-            || bouncing_ball.position.x <= bouncing_ball.radius {
+                || bouncing_ball.position.x <= bouncing_ball.radius
+            {
                 bouncing_ball.velocity.x *= -1f32;
             }
             if bouncing_ball.position.y >= SCREEN_HEIGHT - bouncing_ball.radius
-            || bouncing_ball.position.y <= bouncing_ball.radius {
+                || bouncing_ball.position.y <= bouncing_ball.radius
+            {
                 bouncing_ball.velocity.y *= -1f32;
             }
         }
@@ -90,7 +124,7 @@ fn main() {
         }
 
         // Handle ball movement [KEYBOARD]
-        {
+        if !is_colliding {
             let mut direction: Vector2 = Vector2::new(0f32, 0f32);
             if rl.is_key_down(KeyboardKey::KEY_UP) {
                 direction.y -= 1f32;
@@ -115,11 +149,29 @@ fn main() {
 
         /* --- DRAW --- */
         let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
+
+        // draw texture
+        d.draw_texture_rec(
+            &texture_ground,
+            Rectangle::new(0f32, 0f32, SCREEN_WIDTH, SCREEN_HEIGHT),
+            Vector2::new(0f32, 0f32),
+            Color::WHITE,
+        );
+        d.draw_texture_v(&texture_tree, Vector2::new(0f32, 0f32), Color::WHITE);
 
         // draw bouncing ball
         {
-            d.draw_circle_v(bouncing_ball.position, bouncing_ball.radius, bouncing_ball.color);
-            d.draw_circle_v(bouncing_ball.position, bouncing_ball.radius - 2f32, Color::WHITE);
+            d.draw_circle_v(
+                bouncing_ball.position,
+                bouncing_ball.radius,
+                bouncing_ball.color,
+            );
+            d.draw_circle_v(
+                bouncing_ball.position,
+                bouncing_ball.radius - 2f32,
+                Color::WHITE,
+            );
         }
 
         // centered text drawing
@@ -142,12 +194,19 @@ fn main() {
 
         // Handle ball drawing
         {
+            ball.draw(&mut d);
             d.draw_circle_v(ball.position, ball.radius + 2f32, ball.color);
             d.draw_circle_v(ball.position, ball.radius, Color::WHITE);
         }
 
-        d.clear_background(Color::BLACK);
         d.draw_text(VERSION_NAME, 12i32, 12i32, 24i32, Color::RAYWHITE);
+        d.draw_text(
+            &d.get_fps().to_string(),
+            12i32,
+            32i32,
+            24i32,
+            Color::RAYWHITE,
+        );
     }
 }
 
